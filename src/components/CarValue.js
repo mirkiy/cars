1beb06f09bd02cf08fd2c706678d3c9fa10319cd
mirkiy <mirizachar@gmail.com>
2024-03-04 19:54:49 +0000

    total car cost

--- src/components/CarValue.js
+++ src/components/CarValue.js
@@ -1,5 +1,25 @@
+import { useSelector } from 'react-redux';
+import { createSelector } from 'reselect';
+
 function CarValue() {
-  return <div>CarValue</div>;
+  const memoizedCars = createSelector(
+    (state) => state.cars.data,
+    (state) => state.cars.searchTerm,
+    (cars, searchTerm) => {
+      const filteredCars = cars.filter((car) =>
+        car.name.toLowerCase().includes(searchTerm.toLowerCase())
+      );
+      let cost = 0;
+      for (let car of filteredCars) {
+        cost += car.cost;
+      }
+      return cost;
+    }
+  );
+
+  const totalCost = useSelector(memoizedCars);
+
+  return <div className="car-value">Total Cost: £{totalCost}</div>;
 }
 
 export default CarValue;
