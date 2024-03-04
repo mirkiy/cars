d36c7878fbd95ac9556467976609b4a3549cee56
mirkiy <mirizachar@gmail.com>
2024-03-04 10:23:00 +0000

    listing the records

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -4,9 +4,32 @@ function CarList() {
   const cars = useSelector((state) => {
     return state.cars.data;
   });
-  console.log(cars);
+  const handleCarDelete = (car) => {
+    //
+  };
 
-  return <div>CarList</div>;
+  const renderedCars = cars.map((car) => {
+    return (
+      <div key={car.id} className="panel">
+        <p>
+          {car.name} - £{car.cost}
+        </p>
+        <button
+          className="button is-danger"
+          onClick={() => handleCarDelete(car)}
+        >
+          Delete
+        </button>
+      </div>
+    );
+  });
+
+  return (
+    <div className="car-list">
+      {renderedCars}
+      <hr />
+    </div>
+  );
 }
 
 export default CarList;
